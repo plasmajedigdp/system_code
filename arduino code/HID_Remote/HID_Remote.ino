97c05e813dfbc8630afeb851212eeb0d79818b03
Richard <richardwgladdis@hotmail.co.uk>
2019-04-08 01:30:08 +0100

    Improved HID Remote
    Improved HID Remote functionality with button press and hold implementations

--- arduino code/HID_Remote/HID_Remote.ino
+++ arduino code/HID_Remote/HID_Remote.ino
@@ -3,24 +3,35 @@
 SevenSegmentTM1637 displaySET(10, 9); //Setup SET display
 SevenSegmentTM1637 displayROT(8, 7); //Setup ROT display
 
-uint8_t CLK_PIN = 12; //CLK Encoder pin
-uint8_t DT_PIN = 11; //DT Encoder pin
+uint8_t CLK_PIN = 12; //Encoder CLK pin
+uint8_t DT_PIN = 11; //Encoder DT pin
+uint8_t SW_PIN = 2; //Encode Switch pin - must be on an interrupt pin
 
 bool old_clk; //Value used to check encoder rotation
 bool new_clk; //Value used to check encoder rotation
 
-uint16_t throttle_set = 1000; //Throttle value sent to the motor controller
-uint16_t throttle_rot = 1000; //Throttle value changed by the rotary encoder
 uint16_t throttle_limit_up = 2000; //Upper Limit of the throttle
 uint16_t throttle_limit_dn = 1000; //Lower Limit of the throttle
+uint16_t throttle_set = throttle_limit_dn; //Throttle value sent to the motor controller
+uint16_t throttle_rot = throttle_limit_dn; //Throttle value changed by the rotary encoder
 uint8_t throttle_increment = 5; //Real value is double due to the design of the encoder
+
+//Can't use a delay function in an interrupt so must use large counter numbers
+uint32_t button_hold = 0; //Counter to store how long the button has been held
+uint32_t length_hold = 70000; //How long (in clock ticks) before the display is reset
 
 void setup()
 {
+  //Setup Serial Connection
   Serial.begin(115200);
-  
+
+  //Configure Pins
   pinMode(CLK_PIN, INPUT);
   pinMode(DT_PIN, INPUT);
+  pinMode(SW_PIN, INPUT_PULLUP);
+
+  //Attach the interrupt for the encoder button
+  attachInterrupt(digitalPinToInterrupt(SW_PIN), button_press, FALLING);
 
   //Setup display
   displaySET.begin();
@@ -44,7 +55,8 @@ void setup()
   old_clk = digitalRead(CLK_PIN);
 
   //Set initial display value
-  displayROT.print(throttle_rot);
+  update_throttle_set(throttle_limit_dn);
+  update_throttle_rot(throttle_limit_dn);
 }
 
 void loop()
@@ -59,23 +71,82 @@ void loop()
     if(digitalRead(DT_PIN) != new_clk
     && throttle_rot < throttle_limit_up)
     {
-      //Increment throttle_rot value up
-      throttle_rot += throttle_increment;
+      //Increment throttle_rot value up and update
+      update_throttle_rot(throttle_rot + throttle_increment);
     }
     //Check direction and lower limit
     else if(digitalRead(DT_PIN) == new_clk
     && throttle_rot > throttle_limit_dn)
     {
-      //Increment throttle_rot value down
-      throttle_rot -=throttle_increment;
+      //Increment throttle_rot value down and update
+      update_throttle_rot(throttle_rot - throttle_increment);
     }
 
-    displayROT.print(throttle_rot);
+    //Display the new rotation throttle on the rotation display
+    update_throttle_rot(throttle_rot);
     
     //Set olc_clk value for next loop
     old_clk = new_clk;
-
-    //Print current throttle_rot value
-    Serial.println(throttle_rot);
   }
+}
+
+//Interrupt routine when the button is pressed
+void button_press()
+{
+  //While the button is held down - count up
+  while(!digitalRead(SW_PIN)
+  && button_hold < length_hold)
+  {
+    //Increment
+    button_hold++;
+  }
+
+  //If the button is held down long enough
+  if(button_hold >= length_hold)
+  {
+    //Print reset message first
+    Serial.println("~Throttle Reset");
+    
+    //Reset the throttles to minimum
+    update_throttle_set(throttle_limit_dn);
+    update_throttle_rot(throttle_limit_dn);
+  }
+
+  //If button is not held long enough to reset
+  else
+  {
+    //Set current throttle value
+    update_throttle_set(throttle_rot);
+  }
+
+  //Reset hold counter
+  button_hold = 0;
+}
+
+//Updates the value of the rotational encoder throttle
+void update_throttle_rot(uint16_t new_value)
+{
+  //Update to new throttle value
+  throttle_rot = new_value;
+
+  //Set new value on display
+  displayROT.print(throttle_rot);
+
+  //Print current throttle_rot value
+  Serial.print("Current Rotation: ");
+  Serial.println(throttle_rot);
+}
+
+//Updates the value of the set throttle
+void update_throttle_set(uint16_t new_value)
+{
+  //Update to new throttle value
+  throttle_set = new_value;
+
+  //Set new value on display
+  displaySET.print(throttle_set);
+
+  //Print current throttle_set value
+  Serial.print("~Throttle Set: ");
+  Serial.println(throttle_set);
 }
